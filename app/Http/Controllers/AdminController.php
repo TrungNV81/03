d4568ae7805608b47eaf5f20b1d0673d5dece912
thongdcES <thongdc@eplatform.vn>
2018-12-12 15:21:54 +0700

    handle save db in manageMail

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Support\Facades\DB;
-
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use DB;
 class AdminController extends Controller
 {
     public function __construct()
@@ -31,6 +31,37 @@ class AdminController extends Controller
     {
         $dataMail = DB::table('manage_mail')
             ->get();
+        return view("manageMail", ['dataMail' => $dataMail]);
+    }
+
+    public function editMail(Request $data)
+    {
+
+        $arrId = DB::table('manage_mail')
+        ->select('id')
+        ->get();
+
+        for($i=0; $i<count($arrId);$i++)
+        {
+            $y = 0;
+            $status = 'status'.$arrId[$i]->id;
+            if (isset($_POST[$status])) {
+                $y = 1;
+            }
+            else{
+                $y = 0;
+            }
+            DB::table('manage_mail')
+            ->where('id', $arrId[$i]->id)
+            ->update([
+                'email' => $_POST['mail'.$arrId[$i]->id],  'status' => $y,
+            ]);
+        }
+        echo '<script language="javascript">';
+           echo 'alert("Success")';
+           echo '</script>';
+        $dataMail = DB::table('manage_mail')
+        ->get();
         return view("manageMail", ['dataMail' => $dataMail]);
     }
 
