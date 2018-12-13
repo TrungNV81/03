3aa5d5e76e4f915e1b7c5dea1e678d7a03c99675
trungnvES <trungnv@eplatform.vn>
2018-12-13 08:00:16 +0700

    update js

--- public/js/setting.js
+++ public/js/setting.js
@@ -19,14 +19,6 @@ function Update()
             sender: $('#sender').val()
         },
         success : function (result){
-            $('#permission_id' + $('#idUser').val()).text($("#permission_id_select option:selected" ).text());
-            $('#status' + $('#idUser').val()).text($("#status_id_select option:selected" ).text());
-            $('#firstname').prop('disabled', true);
-            $('#lastname').prop('disabled', true);
-            $('#birthdate').prop('disabled', true);
-            $('#Update_InfoUser').prop('disabled', true);
-            $("#permission_id_select").prop('disabled', true);
-            $("#status_id_select").prop('disabled', true);
             setTimeout(function () { alert(result); }, 250);
         }
     });
