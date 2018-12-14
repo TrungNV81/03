14f16622dd4c0057e24acfc2481118ccdc6aaf44
thanhnc <thanhnc@eplatform.vn>
2018-12-14 16:28:06 +0700

    Fix layout

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -97,99 +97,96 @@
                 </a>
             </div>
         </div>
-        {{--<div class="col-lg-4 col-md-4">--}}
-            {{--<div class="panel panel-red">--}}
-                {{--<div class="panel-heading">--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="col-xs-3">--}}
-                            {{--<i class="fa fa-edit fa-5x"></i>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-xs-9 text-right">--}}
-                            {{--<div class="huge">{{$totalMail}}</div>--}}
-                            {{--<div>Mail</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                {{--<a href="{{ url('/manageMail') }}">--}}
-                    {{--<div class="panel-footer">--}}
-                        {{--<span class="pull-left">View Details</span>--}}
-                        {{--<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>--}}
-                        {{--<div class="clearfix"></div>--}}
-                    {{--</div>--}}
-                {{--</a>--}}
-            {{--</div>--}}
-        {{--</div>--}}
     </div>
     <!-- /.row -->
     <div class="row">
-        <div class="col-lg-12 col-md-12">
+        <div class="col-lg-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <i class="fa fa-table fa-fw"></i>History import file
+                    <i class="fa fa-bar-chart-o fa-fw"></i> Line Chart
+                    <div class="pull-right">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                                Actions
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu pull-right" role="menu">
+                                <li><a href="#">Day</a>
+                                </li>
+                                <li><a href="#">Week</a>
+                                </li>
+                                <li><a href="#">Month</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a href="#">Year</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <!-- /.panel-heading -->
                 <div class="panel-body">
-                    <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example-1">
-                        <thead>
-                        <tr>
-                            <th>File Name</th>
-                            <th>Date</th>
-                            <th>Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($historyFile as $data1)
-                            <tr class="odd gradeX">
-                                <td> {{ $data1->file_name }} </td>
-                                <td> {{ $data1->created_at }} </td>
-                                <td> {{ $data1->status }} </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.panel-body -->
-            </div>
-        </div>
-    </div>
-    <!-- /.row -->
-    <div class="row">
-        <div class="col-lg-12 col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="fa fa-table fa-fw"></i>History send mail
-                </div>
-                <!-- /.panel-heading -->
-                <div class="panel-body">
-                    <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example-2">
-                        <thead>
-                        <tr>
-                            <th>Receiver</th>
-                            <th>File Zip</th>
-                            <th>Date</th>
-                            <th>Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($historySendMail as $data2)
-                            <tr class="odd gradeX">
-                                <td> {{ $data2->receiver }} </td>
-                                <td> {{ $data2->file_zip }} </td>
-                                <td> {{ $data2->created_at }} </td>
-                                <td> {{ $data2->status }} </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                    <div id="morris-line-chart"></div>
                 </div>
                 <!-- /.panel-body -->
             </div>
             <!-- /.panel -->
-        <!-- /.panel -->
         </div>
         <!-- /.col-lg-8 -->
+        <div class="col-lg-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-bell fa-fw"></i> Notifications Panel
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item">
+                            <i class="fa fa-calendar fa-fw"></i> Last Import File
+                            <span class="pull-right text-muted small"><em>4 minutes ago</em>
+                                    </span>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <i class="fa fa-file fa-fw"></i> File Import Today
+                            <span class="pull-right text-muted small"><em>30 files</em>
+                                    </span>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <i class="fa fa-envelope fa-fw"></i> Total Email Edit
+                            <span class="pull-right text-muted small"><em>40 files</em>
+                                    </span>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <i class="fa fa-desktop fa-fw"></i> Last Login
+                            <span class="pull-right text-muted small"><em>43 minutes ago</em>
+                                    </span>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <i class="fa fa-upload fa-fw"></i> Server Rebooted
+                            <span class="pull-right text-muted small"><em>11:32 AM</em>
+                                    </span>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <i class="fa fa-bolt fa-fw"></i> Server Crashed!
+                            <span class="pull-right text-muted small"><em>11:13 AM</em>
+                                    </span>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <i class="fa fa-warning fa-fw"></i> Server Not Responding
+                            <span class="pull-right text-muted small"><em>10:57 AM</em>
+                                    </span>
+                        </a>
+                    </div>
+                    <!-- /.list-group -->
+                    <a href="#" class="btn btn-default btn-block">View All Alerts</a>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-4 -->
     </div>
-    <!-- /.row -->
 </div>
+
 <!-- /#page-wrapper -->
 @endsection
