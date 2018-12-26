76d566ee1281e4b8927f2d18421c2fcf49c103b5
thanglq <thanglq@eplatform.vn>
2018-12-26 13:35:35 +0700

    add modal

--- resources/views/manageMail.blade.php
+++ resources/views/manageMail.blade.php
@@ -53,7 +53,7 @@
                                         <td>
                                             {{-- <form action="{{ url('manageMail') }}" method="GET"> --}}
                                             {{-- <input type="hidden" value="{{ $dataGroup->id }}" name="id_group"> --}}
-                                                <button type="" class="btn btn-warning center-block" onclick="viewMail({{ $dataGroup->id }})"><i class="fa fa-eye fa-fw"></i> View mail</button>
+                                                <button type="" class="btn btn-warning center-block" onclick="viewMail({{ $dataGroup->id }})" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-eye fa-fw"></i> View mail</button>
                                             {{-- </form> --}}
                                         </td>
                                         <td>
@@ -67,10 +67,7 @@
                                     @endforeach
                                 </tbody>
                                 </table>
-                                <hr>
-                                <div>
-                                    <div id="data-mail"></div><br>
-                                </div>
+                                
                                 {{-- @endif --}}
                             </div>
                             <!-- /.col-lg-6 (nested) -->
@@ -86,5 +83,26 @@
         <!-- /.row -->
     </div>
     <!-- /#page-wrapper -->
-
+	<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">List Mail</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+		<div>
+			<div id="data-mail"></div><br>
+		</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
