9441091e68e9dcab56107b9f1eb1e54a190627bd
trungnvES <trungnv@eplatform.vn>
2019-01-08 13:56:24 +0700

    fix error filename

--- app/Http/Services/HomeService.php
+++ app/Http/Services/HomeService.php
@@ -864,10 +864,9 @@ class HomeService
                 foreach ($files as $file) {
                     $position = strrpos($file, '/');
                     $nameFile = substr($file, $position + 1);
-                    // check file name UTF-8 bom
-                    if(substr($nameFile, 0,3) == pack("CCC",0xef,0xbb,0xbf)) {
-                        $nameFile = substr($nameFile, 3);
-                    }
+                    $nameFile = mb_convert_encoding($nameFile, "UTF-8", "auto");
+                    // $utf8_with_bom = chr(239) . chr(187) . chr(191) . $nameFile;
+                    // $utf8_with_bom = iconv(mb_detect_encoding($nameFile, mb_detect_order(), true), "UTF-8", $nameFile);
                     $zip->addFile($zip_folder . $nameDir . '/' . $nameFile);
                     continue;
                 }
